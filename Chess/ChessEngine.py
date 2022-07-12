41dc434d832ca42f702736e840aa8228872a28ec
Ben Halim <bhalim1994@gmail.com>
2022-07-11 17:51:27 -0700

    Starting to implement valid moves. Implemented moveId with simple hashing to override equality operator to check for same moveId instead of same object.

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -40,6 +40,41 @@ class GameState():
             self.board[move.endRow][move.endCol] = move.pieceCaptured
             self.whiteToMove = not self.whiteToMove  # Switch turns back
 
+    '''
+    All moves considering checks.
+    '''
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()  # For now, we won't worry about checks
+
+    '''
+    All moves without considering checks.
+    '''
+    def getAllPossibleMoves(self):
+        moves = [Move((6, 4), (4, 4), self.board)]
+        for r in range(len(self.board)):  # Number of rows
+            for c in range(len(self.board[r])):  # Number of columns in a given row
+                turn = self.board[r][c][0]  # Get the first character (Ie. color)
+                if (turn == 'w' and self.whiteToMove) and (turn == 'b' and not self.whiteToMove):
+                    piece = self.board[r][c][1]
+                    if piece == 'p':
+                        self.getPawnMoves(r, c, moves)
+                    elif piece == 'R':
+                        self.getRookMoves(r, c, moves)
+        return moves
+
+    '''
+    Get all the pawn moves for the pawn located at row, col and add these moves to the list.
+    '''
+    def getPawnMoves(self, r, c, moves):
+        pass
+
+    '''
+    Get all the pawn moves for the pawn located at row, col and add these moves to the list.
+    '''
+    def getRookMoves(self, r, c, moves):
+        pass
+
+
 class Move():
     # Maps keys to values
     # Key : value
@@ -59,6 +94,16 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveId = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+        print(self.moveId)
+
+    '''
+    Overriding the equals method.
+    '''
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveId == other.moveId
+        return False
 
     def getChessNotation(self):
         return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -29,10 +29,12 @@ def main():
     clock = p.time.Clock()
     screen.fill(p.Color("white"))
     gs = ChessEngine.GameState()
+    validMoves = gs.getValidMoves()
+    moveMade = False  # Flag variable for when a move is made
     loadImages()  # Only do this once, before the while loop
     running = True
     sqSelected = ()  # Keeps track last click of user (tuple: (row, col)). No square selected initially.
-    playerClicks = []  # Keeps track of player clicks (two tuples: [(6, 4), (4, 4)]),
+    playerClicks = []  # Keeps track of player clicks (two tuples: [(6, 4), (4, 4)]).
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
@@ -52,7 +54,9 @@ def main():
                 if len(playerClicks) == 2:
                     move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    gs.makeMove(move)
+                    if move in validMoves:
+                        gs.makeMove(move)
+                        moveMade = True
                     sqSelected = ()  # Reset user clicks
                     playerClicks = []  # Clear player clicks
 
@@ -60,6 +64,10 @@ def main():
             elif e.type == p.KEYDOWN:
                 if e.key == p.K_z:  # Undo when 'z' is pressed
                     gs.undoMove()
+                    moveMade = True
+        if moveMade:
+            validMoves = gs.getValidMoves()
+            moveMade = False
 
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
